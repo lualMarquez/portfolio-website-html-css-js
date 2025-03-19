4776c7d90a1122b0eeb08e5730aae681674682ad
Luis Marquez Lopez <luismarquez@Luiss-MacBook-Pro.local>
2025-03-18 20:10:17 -0700

    Updated portfoli website

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function toggleMenu(){
+function toggleMenu() {
+    console.log("Toggle menu called");
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+   
 }
